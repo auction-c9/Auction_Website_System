9b143756cecc5413a9ba97f980df90ea23c54829
phuongthao0612 <phuong6thao@gmail.com>
2025-03-14 01:11:43 +0700

    update

---
+++ auction_website_system/auction_management/src/main/java/com/example/auction_management/dto/TransactionDTO.java
@@ -0,0 +1,19 @@
+package com.example.auction_management.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionDTO {
+    private Integer id;
+    private Integer customerId;
+    private Integer auctionId;
+    private Double amount;
+    private String paymentMethod;
+    private String status;
+}
