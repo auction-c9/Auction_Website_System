0e64efa50239902ca92812100e518f7b97516499
phuongthao0612 <phuong6thao@gmail.com>
2025-03-25 12:56:01 +0700

    admin

--- auction_website_system/auction_management/src/main/java/com/example/auction_management/controller/AdminController.java
+++ auction_website_system/auction_management/src/main/java/com/example/auction_management/controller/AdminController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -79,12 +80,11 @@ public class AdminController {
     @GetMapping("/products/all")
     public ResponseEntity<Page<Product>> getAllProductsForAdmin(
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "5") int size) {
+            @RequestParam(defaultValue = "10") int size) {
 
-        Pageable pageable = PageRequest.of(page, size);
-        Page<Product> products = productService.getAllProducts(pageable); // Lấy tất cả sản phẩm
-
-        return new ResponseEntity<>(products, HttpStatus.OK);
+        Pageable pageable = PageRequest.of(page, size, Sort.by("productId").descending());
+        Page<Product> products = productService.getAllProducts(pageable);
+        return ResponseEntity.ok(products);
     }
 
     @GetMapping("/user-statistics")
--- auction_website_system/auction_management/src/main/java/com/example/auction_management/repository/ProductRepository.java
+++ auction_website_system/auction_management/src/main/java/com/example/auction_management/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import com.example.auction_management.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +15,10 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     List<Product> findByAccountAndIsDeletedFalse(Account account);
     List<Product> findAllByIsDeletedFalse();
     Optional<Product> findByProductIdAndIsDeletedFalse(Integer id);
-    Page<Product> findByIsDeletedFalse(Pageable pageable);
-
+    @Query(value = "SELECT p FROM Product p " +
+            "LEFT JOIN FETCH p.account a " +
+            "LEFT JOIN FETCH a.customer " +
+            "LEFT JOIN p.category c",
+            countQuery = "SELECT COUNT(p) FROM Product p")
+    Page<Product> findAllWithDetails(Pageable pageable);
 }
